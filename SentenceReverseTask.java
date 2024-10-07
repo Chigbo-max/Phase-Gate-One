d92ec1cae3ec2c727bdaca750cb06eb325064981
Chigbo <ezeokeke.chigbo@gmail.com>
2024-10-07 05:48:40 +0100

    updated tasks

--- SentenceReverseTask.java
+++ SentenceReverseTask.java
@@ -5,14 +5,15 @@ public class SentenceReverseTask{
 
 		Scanner userInput = new Scanner(System.in);
 		String sentence = userInput.nextLine();
-		String split[] = sentence.split("");
+		String split[] = sentence.split(" ");
 		displayReversedString(split);
 	}
 
 	public static void displayReversedString(String[] input){
-		for(int count = input.length-1; count >= 0; count--){
+		for(int count = input.length - 1; count >= 0; count--){
 		System.out.print(input[count] + " ");
 			}
+
 	}
 }
 
