85829bb9e183536e7b0576b79184533955818e91
Chigbo <ezeokeke.chigbo@gmail.com>
2024-10-10 12:10:03 +0100

    updated task

--- YearToCenturyConverter.java
+++ YearToCenturyConverter.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class YearToCenturyConverter{
 
-	public int convertYearToCentury(int year){
+	public static void main(String... args){
+		Scanner userInput = new Scanner(System.in);
+		int year = userInput.nextInt();
+		convertYearToCentury(year);
+		isNegative(year);
+
+		String stringYear = userInput.nextLine();
+		isNonInteger(stringYear);
+		
+		
+
+
+	}
+
+	public static int convertYearToCentury(int year){
 
 	int result = 1 +(year/100);
 	
@@ -12,12 +28,14 @@ public class YearToCenturyConverter{
 
 	}
 
-	public void isNegative(int year){
-		if(year <= 0) throw new IllegalArgumentException("invalid input. Year cannot be negative!");
+	public static void isNegative(int year){
+		if(year <= 0)throw new IllegalArgumentException("invalid input. Year cannot be negative!");
 	}
 
-	public void isNonInteger(String year){
-		if(year == "abc")throw new IllegalArgumentException("Input must be an integer");
+
+	public static void isNonInteger(String year){
+	
+	if(year == "abc")throw new IllegalArgumentException("Input must be an integer");
 
 	}
 
